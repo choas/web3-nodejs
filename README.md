34a3bdadb690d5eec50e57435bcb35e1b066872d
Lars Gregori <138015+choas@users.noreply.github.com>
2021-12-11 17:47:14 +0100

    bring this example to 2021
    - testrpc is now ganache-cli
    - web3 version 1.6.1
    - nodemon as devDependency
    - adjust code to show balance for all accounts

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # web3-nodejs
-Simple nodejs web3 example
 
-### Develop
-```
-testrpc
+Simple nodejs [web3](https://github.com/ChainSafe/web3.js) example.
+
+## Develop
+
+```shell
+[ganache-cli](https://www.npmjs.com/package/ganache-cli)
 npm i
 npm start
 ```
--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const run = async () => {
   // Get web3
   const Web3 = require('web3')
-  const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
+  const web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:8545'))
 
-  // Get address
+  // Get accounts
   const accounts = await web3.eth.getAccounts()
-  console.dir(accounts)
 
-  // get balance
-  const balance = await web3.eth.getBalance(accounts[0])
-  console.log(`balance : ${balance}`)
+  // Get balance
+  accounts.forEach( async (account) => {
+    const balance = await web3.eth.getBalance(account)
+    console.log(`${account} - balance: ${balance}`)
+  })
 }
 
 run()
